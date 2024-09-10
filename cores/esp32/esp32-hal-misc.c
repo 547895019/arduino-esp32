585f37385239761f5ab208d7d0c6e57e68d9bf19
547895019 <547895019@qq.com>
2024-09-10 16:45:20 +0800

    移除配置日记

--- cores/esp32/esp32-hal-misc.c
+++ cores/esp32/esp32-hal-misc.c
@@ -271,7 +271,7 @@ void initArduino() {
     }
   }
 #endif
-  esp_log_level_set("*", CONFIG_LOG_DEFAULT_LEVEL);
+  //esp_log_level_set("*", CONFIG_LOG_DEFAULT_LEVEL);
   esp_err_t err = nvs_flash_init();
   if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
     const esp_partition_t *partition = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_NVS, NULL);
